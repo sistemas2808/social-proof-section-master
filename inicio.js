d07ab46f56552c411bef23ad97c00545e4cb0381
sistemas2808 <sistemas2808@gmail.com>
2022-04-27 13:33:30 -0500

    social-proof-section-master
    Reto1

---
+++ inicio.js
@@ -0,0 +1,12 @@
+const Component = () => (
+    <div style="margin: 10px;">
+    <FormWithButton
+    btn="{<button type="submit">Done</button>}
+    />
+    <a onclick={() => redirect()}>
+        <>
+        Text
+        </>
+        </a>
+    </div>
+)
